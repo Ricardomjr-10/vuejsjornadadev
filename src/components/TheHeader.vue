09b419995225860c76f1fcb3c118e3aa31342060
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-06 22:59:06 -0300

    class and styles dinamic

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,4 +1,17 @@
 <template>
+    <h1 class="title">Curso Vue 3</h1>
+    <!-- dinamico -->
+     <h2 :class="classVar">Curso Vue 3</h2>
+     <h3 :class="{'title':false, 'title-home':isHome}">Curso Vue 3</h3>
+
+     <p :class="['text', {'title-home': isHome}]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum optio soluta veniam autem quis assumenda necessitatibus iste reiciendis aliquid nam tempore voluptatibus voluptatem sapiente magnam nulla odio, incidunt cumque accusamus.</p>
+
+     <p :class="pClass">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum optio soluta veniam autem quis assumenda necessitatibus iste reiciendis aliquid nam tempore voluptatibus voluptatem sapiente magnam nulla odio, incidunt cumque accusamus.</p>
+
+     <p style="background-color: black; color: aliceblue;">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam possimus, est ducimus neque in, ea atque magni voluptatibus quia beatae, ipsa nisi? Reprehenderit eum molestiae omnis eaque voluptatum sequi veritatis?</p>
+
+     <p :style="styleClass">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum nulla omnis laborum saepe eaque perspiciatis eligendi non aspernatur consequuntur, id necessitatibus quisquam veritatis voluptate quaerat. Cumque, suscipit necessitatibus. Eum, nisi?</p>
+
 
     <div v-for="(obj, index) in todos" v-bind:key="obj.id" class="todos-item">
         {{ index }} - {{ obj.title }}
@@ -20,6 +33,10 @@ export default {
     name: 'TheHeader',
     data() {
         return {
+            styleClass: {'backgroundColor': 'blue'},
+            isHome: true,
+            pClass: ['text', 'title-home'],
+            classVar: 'title',
             // imgSrc: "https://via.placeholder.com/150",
             // imgAlt: "foto de Jhon Snow",
             todos: [
@@ -62,6 +79,17 @@ export default {
 </script>
 
 <style>
+.text {
+    color: red;
+}
+.title-home{
+    font-size: 40px;
+    color: green;
+}
+.title {
+    font-size: 20px;
+    color: blue;
+}
 .header {
     background: #000;
     color: #fff;
