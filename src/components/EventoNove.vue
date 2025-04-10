8b55e1600e41b4bc958d9883aa81d3b9ff155a57
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-09 21:56:28 -0300

    evento mouse

--- src/components/EventoNove.vue
+++ src/components/EventoNove.vue
@@ -1,15 +1,28 @@
 <template>
 <h1>Aula 9</h1>
 <div>
-    <button v-on:click="onClick">Enviar</button>
-</div>
+    <button v-on:click="onClick">Enviar</button> <!-- v-on ou @ - evento de click>-->
+</div> <br> <br>
+
+<div 
+@mouseover="onMouseOver"
+@mouseout="onMouseOut"
+>
+    Mouse over</div>
+
 </template>
 
 <script>
 export default {
     methods: {
-        onClick() {
-            console.log('click')
+        onClick($evt) {
+            console.log('click', $evt)
+        },
+        onMouseOver($evt) {
+            console.log('mouse over', $evt)
+        },
+        onMouseOut($evt) {
+            console.log('mouse out', $evt)
         }
     }
 
