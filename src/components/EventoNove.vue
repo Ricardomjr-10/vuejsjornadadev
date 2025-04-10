ad657f318145b20f4c75b554e0721a627af67b8a
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-09 21:35:04 -0300

    evento click

--- src/components/EventoNove.vue
+++ src/components/EventoNove.vue
@@ -1,9 +1,17 @@
 <template>
 <h1>Aula 9</h1>
+<div>
+    <button v-on:click="onClick">Enviar</button>
+</div>
 </template>
 
 <script>
 export default {
+    methods: {
+        onClick() {
+            console.log('click')
+        }
+    }
 
 }
 </script>
