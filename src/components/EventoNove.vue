dfadda4c41111d212db334a4ab93aaed367adc48
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-09 22:12:53 -0300

    evento keyup

--- src/components/EventoNove.vue
+++ src/components/EventoNove.vue
@@ -1,14 +1,21 @@
 <template>
 <h1>Aula 9</h1>
 <div>
-    <button v-on:click="onClick">Enviar</button> <!-- v-on ou @ - evento de click>-->
+    <button v-on:click.once="onClick">Enviar</button> <!-- v-on ou @ - evento de click> o once signifaca so vai clicar uma vez-->
 </div> <br> <br>
 
 <div 
 @mouseover="onMouseOver"
 @mouseout="onMouseOut"
 >
-    Mouse over</div>
+    Mouse over</div> <br> <br>
+
+    <div>
+        <form @submit.prevent="onSubmit" action="https://www.google.com"> <!--Buscar eventos modificadores na documentacao do veujs-->
+            <input @keyup.enter="onKeyUp" type="text">
+            <button type="submit">Submit</button>
+        </form>
+    </div>
 
 </template>
 
@@ -23,6 +30,12 @@ export default {
         },
         onMouseOut($evt) {
             console.log('mouse out', $evt)
+        },
+        onSubmit(evento) {
+            console.log('submit', evento)
+        },
+        onKeyUp(evento) {
+            console.log('keyup', evento)
         }
     }
 
