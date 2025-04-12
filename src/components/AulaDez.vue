b2a1f24e6c7f088885bfbc27b3589ac3c523a2b3
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-11 21:10:15 -0300

    create metodo fullname

--- src/components/AulaDez.vue
+++ src/components/AulaDez.vue
@@ -1,12 +1,26 @@
 <template>
 <div>
     <h1>Propriedade Computada</h1>
+    {{ fullName }} <br>
+    {{ user.first_name }} {{ user.last_name }}
 </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            user: {
+                first_name: 'Jon',
+                last_name: 'Snow'
+            }
+        }
+    },
+    computed: {
+        fullName() {
+            return `${this.user.first_name} ${this.user.last_name}`
+        }
+    }
 }
 </script>
 
