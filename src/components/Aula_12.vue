8c4526c810f5c6f09be8fc3819a08922e6d80c45
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-12 21:59:22 -0300

    aula 12

---
+++ src/components/Aula_12.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="container">
+        <h1>Aula 12 - ciclo de vida (life Cycle)</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    watch: {
+
+    },
+    computed: {
+        
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style>
+.container {
+    width: 500px;
+    margin: auto;
+    text-align: justify;
+}
+</style>
