e2c41a717b388e48a251082db3be9c2a928ab9ad
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-14 20:50:33 -0300

    aula13

---
+++ src/components/Aula_13.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="container">
+        <h1>Aula 13 - components <slot></slot></h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        
+        }
+    },
+    watch: {
+
+    },
+    computed: {
+        
+    },
+    methods: {
+
+    },
+}
+</script>
+
+<style>
+.container {
+    width: 500px;
+    margin: auto;
+    text-align: justify;
+}
+</style>
