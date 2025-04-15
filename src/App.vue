4ca08fd7aadbf5c7ea4a8035921e9c1be43f4264
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-14 21:32:41 -0300

    soped e global css style

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <Aula_14/>
+   
   </div>
 </template>
 
 <script>
-import Aula_14 from './components/Aula_14.vue';
+
 
 export default {
   name: 'App',
   components: {
-    Aula_14
+ 
   },
   data () {
     return {
--- src/components/Aula_14.vue
+++ src/components/Aula_14.vue
@@ -1,6 +1,7 @@
 <template>
-    <div>
+    <div class="card">
         <h1>Scoped e Global CSS</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero reiciendis voluptatum officia corrupti consectetur, voluptates, dignissimos qui odio ipsam cumque corporis quasi harum vel recusandae excepturi voluptas dolorum vitae? Dicta.
     </div>
 </template>
 
@@ -10,6 +11,13 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.card {
+    background: bisque;
+    padding:10px;
+    border-radius: 10px;
+    text-align: center;
+    width: 500px;
+    margin:100px auto
+}
 </style>
