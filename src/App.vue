66b2e716625947c09f456a4812505c220433eb1b
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-14 21:38:52 -0300

    props

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
-   
+   <Aula_15/>
   </div>
+  <p>paragrafo</p>
 </template>
 
 <script>
+import Aula_15 from './components/Aula_15.vue';
+
 
 
 export default {
   name: 'App',
   components: {
- 
+    Aula_15
   },
   data () {
     return {
@@ -21,5 +24,10 @@ export default {
 </script>
 
 <style>
-
+p{
+  background: blueviolet;
+  width: 100px;
+  margin: auto;
+  text-align: center;
+}
 </style>
---
+++ src/components/Aula_15.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <h1>Enviando dados para o componente filho (props)</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.container {
+    width: 500px;
+    margin: 100px auto;
+    background: bisque;
+    padding: 10px;
+    border-radius: 10px;
+}
+</style>
