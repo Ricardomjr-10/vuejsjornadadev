82e1ee28a4928b4e34144ae74d8e630dd221f943
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-16 21:22:53 -0300

    aula 16

---
+++ src/components/Aula_16.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <h1>Aula 16 - Enviando dados do pai pro filho</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style>
+.container {
+    width: 500px;
+    margin: 100px auto;
+    background: bisque;
+    padding: 10px;
+    border-radius: 10px;
+}
+</style>
