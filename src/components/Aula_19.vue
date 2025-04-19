7400c913bf321fd9604ca45acd1e935db79ca188
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-19 10:56:41 -0300

    instalacao do vuex

--- src/components/Aula_19.vue
+++ src/components/Aula_19.vue
@@ -1,5 +1,23 @@
 <template>
     <div>
         <h1>Introdução ao Vuex (state)</h1>
+        <p>
+            Quando usar o state - pegar dados do usuario.
+            Vuex - banco de dados do front end.
+            instalação - vue add vuex.
+            invez de colocar os dados no return do data, colocar
+            no state do vuex no store, index.
+            e fica acessivel de qualquer lugar.
+        </p>
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+          
+        }
+    }
+}
+</script>
