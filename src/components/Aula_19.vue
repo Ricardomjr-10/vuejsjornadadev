a4beadfd886dc01178c5ae415bf374b1ab1f7c62
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-19 11:07:51 -0300

    exemplo de dados no state

--- src/components/Aula_19.vue
+++ src/components/Aula_19.vue
@@ -9,15 +9,30 @@
             no state do vuex no store, index.
             e fica acessivel de qualquer lugar.
         </p>
+        
+            <h2>Dados do usuario</h2>
+            <p>{{ $store.state.user }}</p>
+            <p>{{ $store.state.user.first_name }} {{ $store.state.user.last_name }} <br>
+                {{ $store.state.user.email }}
+            </p>
+        
+        <p>
+            {{ $store }}
+        </p>
     </div>
 </template>
 
 <script>
+
 export default {
     data() {
         return {
           
         }
+    },
+    created() {
+        console.log(this.$store)
+        console.log(this.$store.state.user)
     }
 }
 </script>
---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    user: {
+      first_name: 'Ricardo',
+      last_name: 'Rocha',
+      email: 'ricardomjr2@gmail.com'
+    },
+    produtos: [],
+    clientes: []
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
