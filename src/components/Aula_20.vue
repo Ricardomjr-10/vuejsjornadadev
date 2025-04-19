5309ea2c5749a2423b3d4693990a6111cfd7efa0
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-19 11:11:16 -0300

    mutations part 1

---
+++ src/components/Aula_20.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Introdução ao Vuex - Mutations - parte 1</h1>
+        
+            <h2>Dados do usuario</h2>
+            <p>{{ $store.state.user }}</p>
+            <p>{{ $store.state.user.first_name }} {{ $store.state.user.last_name }} <br>
+                {{ $store.state.user.email }}
+            </p>
+        
+        <p>
+            {{ $store }}
+        </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+          
+        }
+    },
+    created() {
+      
+    }
+}
+</script>
