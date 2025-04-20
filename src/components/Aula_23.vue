7507a7ed28f79659d6420835346e5f5092c251d8
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-20 16:47:29 -0300

    vuex actions perfil

--- src/components/Aula_23.vue
+++ src/components/Aula_23.vue
@@ -1,5 +1,26 @@
 <template>
     <div>
         <h1>Aula - 23 introducao ao Vuex - Actions </h1>
+        <p>se parece com as mutations.
+            actions - retorna uma promessa - é assincrono
+        </p>
+        <button @click="updateUser">
+            Atualizar Perfil
+        </button>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        updateUser() {
+            const newUser = {
+            first_name: 'Breno',
+            last_name: 'Muller',
+            email: 'breno@breno.com'
+          }
+          this.$store.commit('storeUser', newUser)
+        }
+    }
+}
+</script>
