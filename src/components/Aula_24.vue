c39f0edb0f17e1c021decad077ac1efaf9b616c1
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-20 21:45:26 -0300

    option api

--- src/components/Aula_24.vue
+++ src/components/Aula_24.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
         <h1>Aula - 24 - introducao a composition API setup</h1>
+        <p>
+            agente tava usando option api, agora usar composition api.
+            com option api utiliza se o mixin - uma script para com logica para ser usado em qualquer lugar. mas ao criar muitos fica dificil saber oque cada um faz.
+        </p>
+        {{ name }}
     </div>
 </template>
+
+<script>
+//option APi
+export default {
+    data() {
+        return {
+            name: 'Ricardo'
+        }
+    }
+}
+</script>
