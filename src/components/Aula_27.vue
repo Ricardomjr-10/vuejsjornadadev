78ff5a335bbc51006e625d711d5246386b599eda
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-22 21:09:16 -0300

    composition api hooks

--- src/components/Aula_27.vue
+++ src/components/Aula_27.vue
@@ -1,5 +1,16 @@
 <template>
     <div>
         <h1>Aula 27 - composition api - ciclo de vida(hooks)</h1>
+        <p>
+
+        </p>
     </div>
 </template>
+
+<script>
+export default {
+    setup() {
+
+    },
+}
+</script>
