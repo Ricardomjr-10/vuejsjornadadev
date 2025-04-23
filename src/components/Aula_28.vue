deba4321579cb06bf66e3cc00b0dc74997a799b6
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-22 21:38:21 -0300

    composition api props

--- src/components/Aula_28.vue
+++ src/components/Aula_28.vue
@@ -1,13 +1,26 @@
 <template>
     <div>
         <h1>Aula 28 - introducao a composition api - Props e Emit</h1>
+        <p>
+            <button>
+                {{ variant }}
+                <slot>save</slot>
+            </button>
+        </p>
     </div>
 </template>
 
 <script>
 export default {
-    setup() {
-        
+    props: {
+        variant: {
+            type: String,
+            default: 'Success'
+        }
+    },
+
+    setup(props) {
+        console.log(props.variant)
     }
 }
 </script>
