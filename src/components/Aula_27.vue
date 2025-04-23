0ab1ca99b2bd18182f8a8fddead1d43be8945260
Ricardo-10 <ricardomjr2@gmail.com>
2025-04-22 21:16:39 -0300

    ciclo de vida  hooks

--- src/components/Aula_27.vue
+++ src/components/Aula_27.vue
@@ -8,9 +8,30 @@
 </template>
 
 <script>
+import { 
+    onBeforeMount,
+    onMounted,
+    onBeforeUnmount,
+    onUnmounted
+ } from 'vue';
+
 export default {
     setup() {
+        onBeforeMount(()=>{
+            console.log('onBeforeMount')
+        }),
 
+        onMounted(()=>{
+            console.log('onMounted')
+        }),
+
+        onBeforeUnmount(()=>{
+            console.log('onBeforeUnmount')
+        }),
+
+        onUnmounted(()=> {
+            console.log('onUnmounted')
+        })
     },
 }
 </script>
